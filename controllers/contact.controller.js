d1bbb43f1e6fb2c57b2712037628d62d39bd5ffb
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-02-18 11:48:33 +0530

    delete contact api created

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -78,3 +78,22 @@ export const updateContact = async (req, res) => {
     res.status(400).json({ message: error.message, success: false });
   }
 };
+
+export const deleteContact = async (req, res) => {
+  try {
+    const region = req.baseUrl.includes("/canada") ? "canada" : "india";
+    const Contact = getContactModel(region);
+
+    const { id } = req.params;
+
+    const contact = await Contact.findByIdAndDelete(id);
+
+    if (!contact) {
+      return res.status(404).json({ message: "Contact not found", success: false });
+    }
+    return res.status(200).json({ message: "Contact deleted successfully", success: true });
+  } catch (error) {
+    console.error("Error deleting contact", error);
+    res.status(500).json({ message: "Failed to delete contact", success: false });
+  }
+};
