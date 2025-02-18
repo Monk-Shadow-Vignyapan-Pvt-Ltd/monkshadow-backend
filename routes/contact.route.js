5f5fd5455532be7056841408c2dae6b3636e153d
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-02-18 13:56:19 +0530

    delete contact api add in contact route

--- routes/contact.route.js
+++ routes/contact.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addContact, getContacts,updateContact} from "../controllers/contact.controller.js";
+import { addContact, getContacts,updateContact,deleteContact} from "../controllers/contact.controller.js";
 import isAuthenticated from "../auth/isAuthenticated.js";
 import { singleUpload } from "../middleware/multer.js";
 
@@ -8,5 +8,6 @@ const router = express.Router();
 router.route("/addContact").post( addContact);
 router.route("/getContacts").get( getContacts);
 router.route("/updateContact/:id").post( updateContact);
+router.route("/deleteContact/:id").delete(deleteContact);
 
 export default router;
